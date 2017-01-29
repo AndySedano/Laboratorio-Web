a1922fa6e5f35494c22445a94e2b62b9346de7ce
Andres E Sedano <andresesedano@myMAC.local>
2017-01-28 22:33:56 -0600

    Ya agregue mi modulo al express
    Entre otras cosas, borre jade y lo reemplace por pug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'IoT Lab', message: 'una pagina que pronto estara muy bonita'});
 });
+
+//Para probar pug
+router.get('/pug', function (req, res) {
+  res.render('index', { title: 'Pug', message: 'Pug en lugar de jade because reasons'})
+})
 
 module.exports = router;
