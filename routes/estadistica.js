4107198734c0e1dcf3ce4bb3d6bd96996b4c3711
rafatmyo <rafatmyo@gmail.com>
2017-01-31 19:41:36 -0600

    Carpeta de Estadistica
    Stuff estadistica

--- routes/estadistica.js
+++ routes/estadistica.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var path = require('path');
 
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname + '/../public/estadistica.html'));
+  res.sendFile(path.join(__dirname + '/../public/estadistica/index.html'));
 });
 
 module.exports = router;
