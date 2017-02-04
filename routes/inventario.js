1af435ff0aef04af23ca62f3dc9a63735dadbfee
Andres E Sedano <andresesedano@myMAC.local>
2017-02-04 10:33:55 -0600

    Agurege un folder para el modulo inventario

--- routes/inventario.js
+++ routes/inventario.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var path = require('path');
 
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname + '/../public/inventario.html'));
+  res.sendFile(path.join(__dirname + '/../public/inventario/inventario.html'));
 });
+
+
 
 module.exports = router;
