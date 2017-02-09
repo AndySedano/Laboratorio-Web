a30fc48fa02a2103f5ae3148e978947435e19463
Ulises Torner <utorner@gmail.com>
2017-02-09 17:17:02 -0600

    The uliStuff is here
    Hey, ya puse mis pags webs al gitsesco.

---
+++ public/divulgacion/uliScripts.js
@@ -0,0 +1,10 @@
+function init() {
+	
+	//Oculta el bloquecillo de nueva difusion
+	document.getElementById("nuevaDifBlock").style.display = 'none';
+
+	//Oculta el bloquecillo de la tabla de difuciones
+	document.getElementById("difTableBlock").style.display = 'none';
+	
+}
+
